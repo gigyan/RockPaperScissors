52f83cb51a860ff2264bed9c684a602385c4e769
gigyan <gyasiigyan13@gmail.com>
2022-02-25 18:11:26 -0500

    simple ui

--- gameCode.js
+++ gameCode.js
@@ -1,12 +1,5 @@
 
-let userScore = 0;
-let compScore = 0;
-
-for(let i = 0; i < 5 ; i++){
-
-let response = prompt(" Choose rock, paper, or scissors.");
-let userChoice =  response.toLowerCase();
- 
+//for(let i = 0; i < 5 ; i++){
 
 
 
@@ -14,7 +7,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let number = getRandomInt(3);
+
 
 
  function computerSelect(intChoice) {
@@ -25,15 +18,13 @@ let number = getRandomInt(3);
    
 }
 
-let computerChoice = computerSelect(number);
-
-
+// let match_result = document.querySelector("#match_result");
 
 
 function playgame(user, computer){
     if (user == "rock" && computer == "scissors"){
-    console.log( "You win " + user +" beats " + computer );
-    userScore++;
+    console.log( "You win " + user +" beats " + computer ); // I want to display this to the user
+     userScore++;
     } else if( user == "rock" && computer == "paper"){
         console.log( "You lose " + computer +" beats " + user );
         compScore++;
@@ -62,15 +53,87 @@ function playgame(user, computer){
 
 }
 
-playgame(userChoice, computerChoice);
+let userChoice = "";
+let userScore = 0;
+let compScore = 0;
 
-}
+//Buttons
+let rock_btn = document.querySelector("#rock_btn");
 
-if(userScore > compScore){
-console.log("You won!");
-} else {
-    console.log("You lost");
-}
+rock_btn.onclick = () => {
+userChoice = "rock";
+
+
+let number = getRandomInt(3);
+let computerChoice = computerSelect(number);
+
+console.log(computerChoice);
+
+playgame( userChoice , computerChoice);
+
+let currentUserScore = document.getElementById("currentUserScore");
+console.log(currentUserScore.innerHTML = userScore);   // I figured out how to change the score
+
+
+let currentCompScore = document.getElementById("currentCompScore");
+console.log(currentCompScore.innerHTML = compScore);   // I figured out how to change the score
+
+
+};
+
+
+
+let paper_btn = document.querySelector("#paper_btn");
+
+paper_btn.onclick = () => {userChoice = "paper";
+
+let number = getRandomInt(3);
+let computerChoice = computerSelect(number);
+
+console.log(computerChoice);
+
+playgame( userChoice , computerChoice);
+
+let currentUserScore = document.getElementById("currentUserScore");
+console.log(currentUserScore.innerHTML = userScore);   
+
+
+let currentCompScore = document.getElementById("currentCompScore");
+console.log(currentCompScore.innerHTML = compScore);   
+
+
+
+};
+
+let scissors_btn = document.querySelector("#scissors_btn");
+
+scissors_btn.onclick = () => {userChoice = "scissors";
+
+let number = getRandomInt(3);
+let computerChoice = computerSelect(number);
+
+console.log(computerChoice);
+
+playgame( userChoice , computerChoice);
+
+let currentUserScore = document.getElementById("currentUserScore");
+console.log(currentUserScore.innerHTML = userScore);   
+
+
+let currentCompScore = document.getElementById("currentCompScore");
+console.log(currentCompScore.innerHTML = compScore);   
+
+};
+
+ 
+
+//}
+
+// if(userScore > compScore){
+// console.log("You won!");
+// } else {
+//     console.log("You lost");
+// }
 
 
 
